7282059ba2452e9551206cbdbed3b65e39adee43
Obadejoseph <obadejoseph54321@gmail.com>
2025-07-30 13:33:29 +0100

    Add files via upload

---
+++ promise.js
@@ -0,0 +1,48 @@
+// const calc = (first, second) =>{
+//     return new Promise (( resolve,reject)=>{
+//         resolve(  ( ogo * abeeb))
+//     })  
+// }  
+
+// tinary operator
+// console.log("i am presh");
+// const checkAdult = ( age)=>{
+//     new Promise ( ( resolve,reject)=>{
+//         age< 18? console.log("presh is a minor") : console.log("presh is a adult")
+//     }).then ( ( kalu)=>{
+//         console.log(kalu)
+//     }).catch((err)=>{
+//         console.log(err.message);
+        
+//     })        
+// }
+
+// checkAdult(12)
+// console.log("i am mmesoma");
+
+
+// const generateRandomNumber =( )=>{
+//     new Promise((resolve,reject)=>{
+//         let theArray = [] 
+// for ( let index = 0; index < 10; index++){
+//     theArray.push(Math.floor( Math.random( ) * 11))
+// } 
+// resolve (
+//     (theArray)
+// )
+   
+//     }).then ((kalu)=>{
+//         console.log(kalu)
+        
+//     }).catch((err)=>{
+//         console.log(err.message);
+        
+//     })
+ 
+    
+
+// }
+// // generateRandomNumber 
+// console.log((generateRandomNumber));
+
+
